e99583da5abb6ca245739c18f9400debef71006a
Krishu1550 <vtu10260@veltechuniv.edu.in>
2023-05-03 15:08:48 +0200

    create models

---
+++ client-consumption/UniversityMgmtSystem/Models/ViewModels/CourseVM.cs
@@ -0,0 +1,14 @@
+﻿namespace UniversityMgmtSystemClientConsuming.Models.ViewModels
+{
+	public class CourseVM
+	{
+		public int Id { get; set; }	
+		public string Name { get; set; }
+
+		public DateTime CousreTime { get; set; }
+		public string Teacher { get; set; }
+
+
+
+	}
+}
