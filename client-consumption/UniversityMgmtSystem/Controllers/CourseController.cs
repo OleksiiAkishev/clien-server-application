9d30fea6bbc6c23b03712863f2f0c4cf817f773d
vtu10260@veltechuniv.edu.in <PF5D@eurofins.com>
2023-05-03 09:33:49 +0200

    TimeTable Course Teacher student - add changes

---
+++ client-consumption/UniversityMgmtSystem/Controllers/CourseController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UniversityMgmtSystemClientConsuming.Controllers
+{
+	public class CourseController : Controller
+	{
+		public IActionResult Index()
+		{
+
+			return View();
+		}
+
+	}
+}
---
+++ client-consumption/UniversityMgmtSystem/Controllers/StudentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UniversityMgmtSystemClientConsuming.Controllers
+{
+	public class StudentController : Controller
+	{
+		public IActionResult DashBoard()
+		{
+			return View();
+		}
+	}
+}
