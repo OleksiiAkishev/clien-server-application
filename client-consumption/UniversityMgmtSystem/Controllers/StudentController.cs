6df914822607a6831ee5d7dd5adb8994efaa3ed1
Krishu1550 <vtu10260@veltechuniv.edu.in>
2023-05-04 11:16:55 +0200

    StudentController DashBoardView

--- client-consumption/UniversityMgmtSystem/Controllers/StudentController.cs
+++ client-consumption/UniversityMgmtSystem/Controllers/StudentController.cs
@@ -1,12 +1,39 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace UniversityMgmtSystemClientConsuming.Controllers
 {
 	public class StudentController : Controller
 	{
-		public IActionResult Index()
+		public IActionResult DashBoard()
 		{
+
 			return View();
+		}
+
+		public async Task<IActionResult> GetCourse()
+		{
+			List<Course> courses = new List<Course>();
+			using (HttpClient httpClient = new HttpClient())
+			{
+
+				var response = await httpClient.GetAsync("https://localhost:7003/api/Course/GetCourses");
+
+				if (response.IsSuccessStatusCode)
+				{
+					var content = await response.Content.ReadAsStringAsync();
+
+					courses = JsonConvert.DeserializeObject<List<Course>>(content);
+					return View(courses);
+				}
+
+			}
+			return BadRequest();
+			return View();
+		}
+		public async Task<IActionResult> EnrollCourse(int id)
+		{
+			return View("DashBoard");
 		}
 	}
 }
